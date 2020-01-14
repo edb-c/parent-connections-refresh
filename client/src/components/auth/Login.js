051518ac4a6e99ff108129e9dabc5fc5e8f3657d
edb-c <edb.cofresi@gmail.com>
2020-01-14 01:29:21 -0500

    Add Redux Store, Reducer

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { Fragment, useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  //destructuring
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+      console.log('Submit works ', formData);
+  };
+
+  return (
+    <Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Log In</h1>
+            <p className="lead text-center">Sign in to your account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  placeholder="Email Address"
+                  autoComplete="email-address"
+                  name="email"
+                  value={email}
+                  onChange={e => onChange(e)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                  name="password"
+                  value={password}
+                  onChange={e => onChange(e)}
+                  minLength="6"
+                />
+              </div>
+              <input
+                type="submit"
+                className="btn btn-info btn-block mt-4"
+                value="Login"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </Fragment>
+  );
 };
 
 export default Login;
+
+/**
+ * NOTES:
+ * https://reactjs.org/docs/hooks-state.html
+ * 
+ * 
+ */
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {};
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
